f660900353cb78e20bf26e51521f3d22517aee02
Pasindu Lakmal <nanayakk-cs18027@stu.kln.ac.lk>
2023-05-12 10:45:18 +0530

    ECommerceWeb

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,1 +1,28 @@
+import express from 'express';
+import Products from '../modules/ProdModule.js';
 
+const ProductRouter = express.Router();
+
+ProductRouter.get('/', async (req, res) => {
+  const products = await Products.find();
+  res.send(products);
+});
+
+ProductRouter.get('/slug/:slug', async (req, res) => {
+  const product = await Products.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+ProductRouter.get('/:id', async (req, res) => {
+  const product = await Products.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+export default ProductRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import data from './data.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import SeedRouter from './routes/SeedRoutes.js';
+import ProductRouter from './routes/ProductRoutes.js';
 
 dotenv.config();
 mongoose
@@ -17,28 +18,31 @@ mongoose
 const app = express();
 
 app.use('/api/seed', SeedRouter);
+app.use('/api/products', ProductRouter);
+app.use('/api/products/slug/:slug', ProductRouter);
+app.use('/api/products/:id', ProductRouter);
 //app.use('/api/products', ProductRouter);
-app.get('/api/products', function (req, res) {
+/*app.get('/api/products', function (req, res) {
   res.send(data.products);
-});
+});*/
 
-app.get('/api/products/slug/:slug', function (req, res) {
+/*app.get('/api/products/slug/:slug', function (req, res) {
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
     res.send(product);
   } else {
     res.status(404).send({ message: 'Product Not Found' });
   }
-});
+});*/
 
-app.get('/api/products/:id', function (req, res) {
+/*app.get('/api/products/:id', function (req, res) {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
     res.status(404).send({ message: 'Product Not Found' });
   }
-});
+});*/
 
 const port = process.env.PORT || 5000;
 app.listen(port, function () {
