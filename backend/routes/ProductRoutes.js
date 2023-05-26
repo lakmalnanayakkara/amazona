25ff3beb5954f4e0d0a4ada399d6775dcdef0472
Pasindu Lakmal <nanayakk-cs18027@stu.kln.ac.lk>
2023-05-27 00:15:06 +0530

    ECommerceWeb

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Products from '../modules/ProdModule.js';
+import expressAsyncHandler from 'express-async-handler';
 
 const ProductRouter = express.Router();
 
@@ -7,6 +8,14 @@ ProductRouter.get('/', async (req, res) => {
   const product = await Products.find();
   res.send(product);
 });
+
+ProductRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Products.find().distinct('category');
+    res.send(categories);
+  })
+);
 
 ProductRouter.get('/slug/:slug', async (req, res) => {
   const product = await Products.findOne({ slug: req.params.slug });
