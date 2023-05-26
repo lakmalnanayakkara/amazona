2121e10d891bf598c2b717f2d2954630b559e537
Pasindu Lakmal <nanayakk-cs18027@stu.kln.ac.lk>
2023-05-26 17:00:38 +0530

    ECommerceWeb

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -69,7 +69,7 @@ export default function OrderHistoryScreen() {
               orders.map((order) => (
                 <tr key={order._id}>
                   <td>{order._id}</td>
-                  <td>{order.createdAtsubstring(0, 10)}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
                   <td>{order.totalPrice.toFixed(2)}</td>
                   <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
                   <td>
